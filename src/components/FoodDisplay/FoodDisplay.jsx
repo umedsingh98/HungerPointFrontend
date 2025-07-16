df91f7b4c1d16da300cef1b17437b495e9e27064
umedsingh98 <umedsingh0898@gmail.com>
2025-07-16 09:27:11 +0530

    updated

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,27 +1,30 @@
-import React, { useContext, useState, useEffect } from 'react';
-import '../FoodDisplay/foodDisplay.css';
-import { StoreContext } from '../../context/StoreContext';
-import FoodItem from '../Food Item/FoodItem';
+import React, { useContext, useState, useEffect } from "react";
+import "../FoodDisplay/foodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../Food Item/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
   const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  if (Array.isArray(food_list) && food_list.length > 0) {
-    setLoading(false);
-  }
-}, [food_list]);
+  useEffect(() => {
+    if (Array.isArray(food_list) && food_list.length > 0) {
+      setLoading(false);
+    }
+  }, [food_list]);
 
   return (
     <div className="food-display">
       <h2>Top dishes near you</h2>
       {loading ? (
-        <div className="loader"><h1>Loading...</h1></div>
+        <div className="loader">
+          <div className="spinner"></div>
+          <h3>Loading products...</h3>
+        </div>
       ) : (
         <div className="food-display-list">
           {food_list?.map((item, index) => {
-            if (category === 'All' || category === item.category) {
+            if (category === "All" || category === item.category) {
               return (
                 <FoodItem
                   key={index}
